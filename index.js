cc0c6426ed4bb4b9f44ce4a5000380616c54fb03
jcbhmr <jcbhmr@outlook.com>
2023-12-01 20:33:03 -0600

    save

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-"use strict"
-
-const getBatteryLevel = require("battery-level")
-const getBatteryCharging = require("is-charging")
-
-module.exports = async () => {
-	try {
-		let level = await getBatteryLevel()
-		let charging = await getBatteryCharging()
-
-		// Windows doesn't support desktops which don't have batteries
-		if (Number.isNaN(level)) {
-			level = 1
-			charging = true
-		}
-
-		return {
-			level,
-			charging
-		}
-	} catch (_) {
-		return {
-			level: 1,
-			charging: true
-		}
-	}
+navigator.getBattery = async () => {
+    
 }
